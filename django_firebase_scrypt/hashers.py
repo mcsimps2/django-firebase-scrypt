b9606b09df447421eac26dd12674064fc031c585
James Hudson <bugs.feedback.whatever@gmail.com>
2024-07-22 12:52:56 +0200

    Removed deprecated internationalization.

--- django_firebase_scrypt/hashers.py
+++ django_firebase_scrypt/hashers.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 from django.conf import settings
 from django.contrib.auth.hashers import BasePasswordHasher, mask_hash
-from django.utils.translation import ugettext_noop as _
+from django.utils.translation import gettext_noop as _
 
 import pyscryptfirebase
 
